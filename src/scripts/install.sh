575a606881ae720a9d6812b997f0b369324f9993
Brad Sickles <brad.sickles@gmail.com>
2022-02-02 15:50:04 -0500

    Fix executor, add test

--- src/scripts/install.sh
+++ src/scripts/install.sh
@@ -6,36 +6,47 @@ get_latest_version() {
   curl --silent "https://github.com/${repo}/releases/latest" | sed 's#.*tag/\(.*\)\".*#\1#'
 }
 
-Install() {
+install_windows() {
+  version=$1
+  raw_version=${version//v/} # Drop first character 'v'
+  arch=$(go env GOARCH)
+
+  download_url="https://github.com/${repo}/releases/download/${version}/nullstone_${raw_version}_windows_${arch}.zip"
+  echo "Downloading Nullstone CLI from '${download_url}'..."
+  curl -o nullstone.zip -sSL "${download_url}"
+  unzip nullstone.zip
+}
+
+install_other() {
   version=$1
   raw_version=${version//v/} # Drop first character 'v'
   os=$(go env GOOS)
   arch=$(go env GOARCH)
-  if [ "$(go env GOOS)" = "windows" ]; then
-    download_url="https://github.com/${repo}/releases/download/${version}/nullstone_${raw_version}_windows_$(go env GOARCH).zip"
-    echo "Downloading Nullstone CLI from '${download_url}'..."
-    curl -o nullstone.zip -sSL "${download_url}"
-    unzip nullstone.zip
-  else
-    download_url="https://github.com/${repo}/releases/download/${version}/nullstone_${raw_version}_${os}_${arch}.tar.gz"
-    echo "Downloading Nullstone CLI from '${download_url}'..."
-    curl -o nullstone.tar.gz -sSL "${download_url}"
-    tar -xvf nullstone.tar.gz
 
-    echo "Installing Nullstone CLI to /usr/local/bin/nullstone"
-    [ -w /usr/local/bin ] && SUDO="" || SUDO=sudo
-    $SUDO mv nullstone /usr/local/bin/nullstone
-    chmod +x /usr/local/bin/nullstone
+  download_url="https://github.com/${repo}/releases/download/${version}/nullstone_${raw_version}_${os}_${arch}.tar.gz"
+  echo "Downloading Nullstone CLI from '${download_url}'..."
+  curl -o nullstone.tar.gz -sSL "${download_url}"
+  tar -xvf nullstone.tar.gz
+
+  echo "Installing Nullstone CLI to /usr/local/bin/nullstone"
+  [ -w /usr/local/bin ] && SUDO="" || SUDO=sudo
+  $SUDO mv nullstone /usr/local/bin/nullstone
+  chmod +x /usr/local/bin/nullstone
+}
+
+install_os() {
+  os=$(go env GOOS)
+  if [ "${os}" = "windows" ]; then
+    install_windows "$1"
+  else
+    install_other "$1"
   fi
 }
 
-# Will not run if sourced for bats-core tests.
-# View src/tests for more information.
-ORB_TEST_ENV="bats-core"
-if [ "${0#*$ORB_TEST_ENV}" == "$0" ]; then
+Install() {
   if [ ! "$(which nullstone)" ]; then
     latest_version=$(get_latest_version)
-    Install "${latest_version}"
+    install_os "${latest_version}"
   else
     latest_version=$(get_latest_version)
     cur_version="v$(nullstone --version | cut -d ' ' -f3)"
@@ -44,7 +55,14 @@ if [ "${0#*$ORB_TEST_ENV}" == "$0" ]; then
       nullstone --version
     else
       echo "Nullstone CLI is out-of-date, installing latest..."
-      Install "${latest_version}"
+      install_os "${latest_version}"
     fi
   fi
+}
+
+# Will not run if sourced for bats-core tests.
+# View src/tests for more information.
+ORB_TEST_ENV="bats-core"
+if [ "${0#*$ORB_TEST_ENV}" == "$0" ]; then
+  Install
 fi
--- src/tests/install.bats
+++ src/tests/install.bats
@@ -3,3 +3,8 @@ setup() {
     # Load our script file.
     source ./src/scripts/install.sh
 }
+
+@test '1: Install' {
+    Install
+    [ -x "/usr/local/bin/nullstone" ]
+}
