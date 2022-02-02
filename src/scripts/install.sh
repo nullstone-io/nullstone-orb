e20b0447107644b66f3e1dc4c47ba1509c1e28ea
Brad Sickles <brad.sickles@gmail.com>
2022-02-02 15:55:27 -0500

    remove golang dependency for install

--- src/scripts/install.sh
+++ src/scripts/install.sh
@@ -6,22 +6,15 @@ get_latest_version() {
   curl --silent "https://github.com/${repo}/releases/latest" | sed 's#.*tag/\(.*\)\".*#\1#'
 }
 
-install_windows() {
+install() {
   version=$1
   raw_version=${version//v/} # Drop first character 'v'
-  arch=$(go env GOARCH)
 
-  download_url="https://github.com/${repo}/releases/download/${version}/nullstone_${raw_version}_windows_${arch}.zip"
-  echo "Downloading Nullstone CLI from '${download_url}'..."
-  curl -o nullstone.zip -sSL "${download_url}"
-  unzip nullstone.zip
-}
-
-install_other() {
-  version=$1
-  raw_version=${version//v/} # Drop first character 'v'
-  os=$(go env GOOS)
-  arch=$(go env GOARCH)
+  os="linux"
+  arch="amd64"
+  if [ -n "$(uname | grep "Darwin")" ]; then
+      platform="darwin"
+  fi
 
   download_url="https://github.com/${repo}/releases/download/${version}/nullstone_${raw_version}_${os}_${arch}.tar.gz"
   echo "Downloading Nullstone CLI from '${download_url}'..."
@@ -34,28 +27,18 @@ install_other() {
   chmod +x /usr/local/bin/nullstone
 }
 
-install_os() {
-  os=$(go env GOOS)
-  if [ "${os}" = "windows" ]; then
-    install_windows "$1"
-  else
-    install_other "$1"
-  fi
-}
-
 Install() {
+  latest_version=$(get_latest_version)
   if [ ! "$(which nullstone)" ]; then
-    latest_version=$(get_latest_version)
-    install_os "${latest_version}"
+    install "${latest_version}"
   else
-    latest_version=$(get_latest_version)
     cur_version="v$(nullstone --version | cut -d ' ' -f3)"
     if [ "${latest_version}" = "${cur_version}" ]; then
       echo "Nullstone CLI is already installed, skipping installation."
       nullstone --version
     else
       echo "Nullstone CLI is out-of-date, installing latest..."
-      install_os "${latest_version}"
+      install "${latest_version}"
     fi
   fi
 }
