bc045c802934d72606116602833e971d1f2ab0aa
Brad Sickles <brad.sickles@gmail.com>
2022-05-26 10:27:48 -0400

    [semver:patch] Fixed usage of env vars for app and env flags.

---
+++ src/scripts/push.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -e
+
+Push() {
+    CMD="nullstone push --source=${PARAM_SOURCE}"
+    if [ -n "${PARAM_APP}" ]; then
+        CMD="${CMD} --app=${PARAM_APP}"
+    fi
+    if [ -n "${PARAM_ENV}" ]; then
+        CMD="${CMD} --env=${PARAM_ENV}"
+    fi
+    if [ -n "${PARAM_VERSION}" ]; then
+        CMD="${CMD} --version=${PARAM_VERSION}"
+    fi
+    eval "${CMD}"
+}
+
+# Will not run if sourced from another script. This is done so this script may be tested.
+# View src/tests for more information.
+if [[ "$_" == "$0" ]]; then
+    Push
+fi
