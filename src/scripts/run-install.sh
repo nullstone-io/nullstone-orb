8d10f95a0f0216ca5802b20d91071b8730488f77
Brad Sickles <brad.sickles@gmail.com>
2023-05-23 07:48:43 -0400

    dumb

--- src/scripts/run-install.sh
+++ src/scripts/run-install.sh
@@ -1,8 +1,4 @@
 #!/bin/bash -e
 
-SCRIPT_DIR=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
-# shellcheck source=/dev/null
-#source "${SCRIPT_DIR}/install.sh"
 source "./src/scripts/install.sh"
-
 Install
