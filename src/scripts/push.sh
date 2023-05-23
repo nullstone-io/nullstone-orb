1c4b7c9c71d6f426fef8315bace00ed7e65bfac1
Brad Sickles <brad.sickles@gmail.com>
2023-05-23 08:21:05 -0400

    Add `stack` param to push/deploy commands

--- src/scripts/push.sh
+++ src/scripts/push.sh
@@ -2,6 +2,9 @@
 
 Push() {
     CMD="nullstone push --source=${PARAM_SOURCE}"
+    if [ -n "${PARAM_STACK}" ]; then
+        CMD="${CMD} --stack=${PARAM_STACK}"
+    fi
     if [ -n "${PARAM_APP}" ]; then
         CMD="${CMD} --app=${PARAM_APP}"
     fi
