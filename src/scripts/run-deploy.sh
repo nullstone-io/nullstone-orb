8baa17c53c9ad44e866924228fbe0917a550737a
Brad Sickles <brad.sickles@gmail.com>
2023-05-23 07:26:27 -0400

    Add comments for shellcheck

--- src/scripts/run-deploy.sh
+++ src/scripts/run-deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -e
 
+# shellcheck source=./deploy.sh
 source $(dirname "${BASH_SOURCE[0]}")/deploy.sh
 Deploy
--- src/scripts/run-push.sh
+++ src/scripts/run-push.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -e
 
+# shellcheck source=./push.sh
 source $(dirname "${BASH_SOURCE[0]}")/push.sh
 Push
