86fbae7ebd384c49c95d8b8310d680bb4d135c78
Brad Sickles <brad.sickles@gmail.com>
2023-05-23 07:47:46 -0400

    try a couple things

--- src/scripts/run-deploy.sh
+++ src/scripts/run-deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -e
 
 SCRIPT_DIR=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
+# shellcheck source=/dev/null
 source "${SCRIPT_DIR}/deploy.sh"
 
 Deploy
--- src/scripts/run-install.sh
+++ src/scripts/run-install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash -e
 
 SCRIPT_DIR=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
-source "${SCRIPT_DIR}/install.sh"
+# shellcheck source=/dev/null
+#source "${SCRIPT_DIR}/install.sh"
+source "./src/scripts/install.sh"
 
 Install
