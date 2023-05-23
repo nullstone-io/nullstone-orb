caa77e4e0e64f4727d4ae44b761433324942b617
Brad Sickles <brad.sickles@gmail.com>
2023-05-23 08:35:50 -0400

    fix shellcheck

--- src/scripts/run-deploy.sh
+++ src/scripts/run-deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -e
 
+# shellcheck source=./src/scripts/deploy.sh
 source "$(dirname "${BASH_SOURCE[0]}")/deploy.sh"
 Deploy
--- src/scripts/run-push.sh
+++ src/scripts/run-push.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -e
 
+# shellcheck source=./src/scripts/push.sh
 source "$(dirname "${BASH_SOURCE[0]}")/push.sh"
 Push
